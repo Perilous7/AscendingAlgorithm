2b28e53d25d97a1d0d6be0f185b1f36b8e8a3f59
perilous7 <ruichen.zhou82@gmail.com>
2020-06-12 14:17:34 -0400

    fix coin1

--- src/DynamicProgramming/CoinChange1.java
+++ src/DynamicProgramming/CoinChange1.java
@@ -4,18 +4,19 @@ import java.util.Arrays;
 
 public class CoinChange1 {
     public static int coinChange(int[] coins, int amount) {
-        Arrays.sort(coins);
-        int[] dp = new int[amount+1];
-        Arrays.fill(dp,amount+1);
-        for(int i = 0;i<=amount;i++){
-            for(int j=0;j<coins.length;j++){
-                if(coins[j]<=i){
-                    dp[i] = Math.min(dp[i],1+dp[i-coins[j]]);
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount+1);
+        dp[0]=0;
+        for (int i = 0; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i >= coin) {
+                    dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
                 }
             }
         }
-        return dp[amount]>amount?-1:dp[amount];
+        return dp[amount]>amount? -1:dp[amount];
     }
+
 
     public static void main(String[] args) {
         int[] coins ={186,419,83,408};
